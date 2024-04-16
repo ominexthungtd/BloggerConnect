e68d23cde669fef3ccc10c1c36a441a47125c134
HungTD <hungtd@ominext.com>
2024-04-16 22:00:00 +0700

    post-logic-2

--- bloggerconnect/posts/models.py
+++ bloggerconnect/posts/models.py
@@ -1,3 +1,70 @@
+import uuid
 from django.db import models
+from users.models import Profile
+from tinymce import models as tinymce_models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(
+        Profile, null=True, blank=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    desc = models.TextField(null=False, blank=False)
+    content = tinymce_models.HTMLField()
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    public = models.BooleanField(default=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def reviewers(self):
+        queryset = self.review_set.all().values_list('owner__id', flat=True)
+        return queryset
+
+    @property
+    def getVoteCount(self):
+        reviews = self.review_set.all()
+        upVotes = reviews.filter(value='up').count()
+        totalVotes = reviews.count()
+
+        ratio = (upVotes / totalVotes) * 100
+        self.vote_total = totalVotes
+        self.vote_ratio = ratio
+        self.save()
+
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ("up", "Up Vote"), ("down", "Down Vote")
+    )
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    class Meta:
+        unique_together = [['owner', 'post']]
+
+    def __str__(self):
+        return self.value
--- bloggerconnect/posts/views.py
+++ bloggerconnect/posts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ReviewForm
+from .models import Tag, Post
+from .utils import searchPosts, paginatePosts
 
-# Create your views here.
+
+def posts(request):
+    posts, search_query = searchPosts(request)
+    custom_range, posts = paginatePosts(request, posts, 6)
+    context = {'posts': posts, 'search_query': search_query,
+               'custom_range': custom_range}
+    return render(request, 'projects/projects.html', context)
+
+
+def post(request, pk):
+    postObj = Post.objects.get(id=pk)
+    form = ReviewForm()
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.post = postObj
+        review.owner = request.user.profile
+        review.save()
+        postObj.getVoteCount
+        messages.success(request, "Your review was successfully submitted!")
+        return redirect('post', pk=postObj.id)
+    return render(request, "projects/single-project.html", {"post": postObj, "form": form})
