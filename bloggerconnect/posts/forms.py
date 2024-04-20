8b83f5206899e0ae87fc821693297a23f5b18bda
HungTD <hungtd@ominext.com>
2024-04-20 22:00:00 +0700

    post-logic-2

---
+++ bloggerconnect/posts/forms.py
@@ -0,0 +1,19 @@
+from django.db.models.base import Model
+from django.forms import ModelForm, widgets
+from django import forms
+from .models import Review
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['value', 'body']
+        labels = {
+            'value': 'Place your vote',
+            'body': 'Add a comment with your vote'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
